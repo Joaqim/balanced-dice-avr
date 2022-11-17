a582115dacc5171e533d8a0e5ffc774092496c12
Joaqim Planstedt <git@joaqim.xyz>
2022-11-17 15:29:43 +0100

    Added unfinished examples to dev branch

--- examples/Attiny85RNG/Attiny85RNG.ino
+++ examples/Attiny85RNG/Attiny85RNG.ino
@@ -54,7 +54,7 @@ void setup()
     // https://github.com/Lorandil/ATTiny85-optimization-guide
     // Initialize ATTiny85's timer 0 to run at full cpu clock (prescaler = 1).
     // The timer will count from its starting value (which does not require to be set for this use case)
-    // to 255 and overflow to 0 and continue counting until the power is cut.
+    // up to 255 and overflow to 0 and continue counting until the power is cut.
     TCCR0A = 0x00;        // normal mode
     TCCR0B = (1 << CS00); // prescaling with 1
 
@@ -69,7 +69,7 @@ static uint8_t n = 0;
 
 void loop()
 {
-    // Since the 'Deck of dice' is only expected to be shuffled occasionally, we can collect random seed at user input
+    // Since the 'Deck of dice' is only expected to be shuffled at start or at input, we can collect random seed at user input
 
     // If we access the timer at user input we get a pseudo random number from timer
     if(userInput()) {
--- examples/DiceDistribution/DiceDistribution.ino
+++ examples/DiceDistribution/DiceDistribution.ino
@@ -1,10 +1,34 @@
 
 #include <BalancedDice.h>
 
+#include <avr/eeprom.h>
+
+#define FRAME_DURATION 14
+
+uint8_t lastTimeStamp, tickNum;
+
 void setup() {
+  randomSeed(analogRead(0));
+}
+
+void updateState() {
+
+}
+
+void draw() {
 
 }
 
 void loop() {
+  uint8_t t, delta_t;
 
+  t = millis();
+  delta_t = t < lastTimeStamp ? t + (0xFF - lastTimeStamp) : t - lastTimeStamp;
+  if (delta_t > FRAME_DURATION) {
+    tickNum++;
+    lastTimeStamp += FRAME_DURATION;
+
+    updateState();
+    draw();
+  }
 }
